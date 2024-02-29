6f29bba8348d46e00aec9a75e76b3340e6aaebd3
BokattaStudio <161750282+BokattaStudio@users.noreply.github.com>
2024-02-29 18:35:27 +0100

    Create index.php
    Hello world in html and Javascript

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World GitHub Test</title>
+</head>
+<body>
+  <h2>Hello there... this is a Hello World GitHub Test, on html and javascript.</h2>
+  <h4>HTML >> Hello World!</h4>
+  <h4>Javascript >> <script>document.write("Hello World!");</script></h4>
+</body>
+</html>
