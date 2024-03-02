f0ee2b087ae3cc1da35740016bdeec167e367d58
BokattaStudio <161750282+BokattaStudio@users.noreply.github.com>
2024-03-02 12:50:37 +0100

    v0.4
    Minor fixes and improvements.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World GitHub Test</title>
+    <title>Hello World GitHub Test v0.4</title>
 </head>
 <body>
   <h2>Hello there... this is a Hello World GitHub Test, on html and javascript.</h2>
